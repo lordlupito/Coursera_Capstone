54e81f88e059b8cbca46a66fd72f71d66008fa08
lordlupito <82732783+lordlupito@users.noreply.github.com>
2021-07-25 18:11:54 -0500

    Add files via upload

---
+++ Coursera_Capstone. .py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera_Capstone
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
